ee79bd4e8aff6b1ee679f74261fbd1b75e700702
Hilal Ahmad <hilaldev123@gmail.com>
2022-10-23 00:42:31 -0700

    Initial Commit

---
+++ controllers/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$pageHeading = 'Home Page';
+
+require 'views/index.view.php';
---
+++ views/about.view.php
@@ -0,0 +1,17 @@
+  <!-- Header  -->
+  <?php require 'partials/header.php'; ?>
+
+  <!-- Navbar  -->
+  <?php require 'partials/nav.php'; ?>
+
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $pageHeading; ?></h1>
+    </div>
+  </header>
+  <main>
+    <p class="text-gray-700 mx-20 my-5">Your are on about page</p>
+  </main>
+
+  <!-- Footer  -->
+  <?php require 'partials/footer.php'; ?>
